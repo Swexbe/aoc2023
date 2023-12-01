f9abd04b5d09a9ec5406212b5efad03a440ed12f
Erik Johnsson <erik@swex.be>
2023-12-01 20:29:10 +0100

    day 1

--- utils/get_input.py
+++ utils/get_input.py
@@ -1,6 +1,6 @@
 import requests as requests
 
-cookie = ""
+cookie = "53616c7465645f5fb20254bdf8e4103871772c4579c2fe23f9deea33d69788b9d9aac1d3676ddaeae35fb0ac4243222003015602b3a5d7af718dca2c7c7be80d"
 
 
 def get_input(day: int, year: int = 2023) -> str:
